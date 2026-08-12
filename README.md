e228f38030df723f6931475e0d1aa50c39e1a64b
Stalhein <erich.stalhein@proton.me>
2026-08-12 21:45:11 +0000

    Added user input for image size and ray samples.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Raytracing in One Weekend implementation in Rust
 Learning the Rust programming language by following the Raytracing in One Weekend tutorial/book but converting from C++ to Rust.
 
+Output images are saved to *image.ppm*.
+
 ## Compilation steps
-Cargo and Rust must be installed on the system before running ```cargo run```.
+Cargo and Rust must be installed on the system before running:
+* ```cargo run```
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,37 @@ use camera::*;
 
 use std::io::Write as IoWrite;
 use std::fs::File;
+use std::io;
+
+fn get_input_int(default: i32) -> i32 {
+    let mut i = String::new();
+    io::stdin().read_line(&mut i).expect("Invalid input - set to {default}");
+    match i.trim().parse() {
+        Ok(num) => { return num; },
+        Err(_) => {
+            println!("Invalid input - set to {default}");
+            return default;
+        }
+    };
+}
 
 fn main() -> std::io::Result<()> {
-    let aspect_ratio: f64 = 16.0 / 9.0;
-    let image_width: i32 = 1920;
+    // Width
+    print!("Enter desired image width (higher takes longer - default is 800): ");
+    let _ = io::stdout().flush();
+    let w = get_input_int(800);
+
+    // Height
+    print!("Enter desired image height (again higher takes longer - default is 600): ");
+    let _ = io::stdout().flush();
+    let h = get_input_int(600);
+
+    // Rpp
+    print!("Enter number of rays per pixel, larger numbers reduce noise but take longer (default is 8): ");
+    let _ = io::stdout().flush();
+    let r = get_input_int(8);
+
+    let aspect_ratio: f64 = w as f64 / h as f64;
 
     let mut buffer = String::new();
 
@@ -22,7 +49,7 @@ fn main() -> std::io::Result<()> {
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera 
-    let camera = Camera::new(image_width, aspect_ratio, 100);
+    let camera = Camera::new(w as i32, aspect_ratio, r as i32);
 
     camera.render(&mut buffer, &world);
 
